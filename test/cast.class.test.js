76b8e3a5f250efc2b4901ebb7ff2a76ce165e427
Svinci131 <svinci131@gmail.com>
2016-06-02 14:46:41 -0400

    added name checking tests

--- test/cast.class.test.js
+++ test/cast.class.test.js
@@ -8,22 +8,36 @@ const should = chai.should();
 const expect = chai.expect;
 const Movie = require ('../scripts/movie');
 const aladdin = new Movie ("Aladdin", [["PEDDLar", 'prince','SULTAN', "jafar", "aladdin"], ["Jasmine"]]);
-
-
+const avengers = new Movie ("Avengers,-The-(2012)", [["AGENT PHIL COULSON", "NICK FURY", "LOKI"], ["AGENT MARIA HILL", "NATASHA"]]);
 
 describe('isName', function () {
-
+	beforeEach(function() {
+        aladdin.cast.setNameVariations();
+    });
 	it('returns true if its a male chars name', function () {
-		aladdin.cast.setNameVariations();
-		console.log("here", aladdin.cast.isName('SULTAN'));
 		expect(aladdin.cast.isName('SULTAN')).to.be.true;
 	});
 	it('returns false if its a female chars name', function () {
-		aladdin.cast.setNameVariations();
 		expect(aladdin.cast.isName('Jasmine')).to.be.true;
 	});
 	it('returns false if its neither', function () {
-		aladdin.cast.setNameVariations();
 		expect(aladdin.cast.isName('fdsafdsa')).to.be.false;
 	});
 });
+
+describe('isFemaleName', function () {
+	beforeEach(function() {
+        avengers.cast.setNameVariations();
+    });
+	it('returns false if its a male chars name', function () {
+		expect(avengers.cast.isFemaleName('Loki')).to.be.false;
+	});
+	it('returns false if its a female chars name', function () {
+		expect(avengers.cast.isFemaleName("AGENT MARIA HILL")).to.be.true;
+	});
+	it('returns false if its neither', function () {
+		expect(avengers.cast.isFemaleName('fdsafdsa')).to.be.false;
+	});
+});
+
+
