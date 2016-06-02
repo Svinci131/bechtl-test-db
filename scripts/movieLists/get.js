3e53ab6311423c8c0faaec455a599527e6dde5dd
Svinci131 <svinci131@gmail.com>
2016-06-02 18:22:33 -0400

    got char and actors

--- scripts/movieLists/get.js
+++ scripts/movieLists/get.js
@@ -4,6 +4,7 @@
 const cheerio = require("cheerio");
 const getData = require("../getData");
 
+var obj = {};
 
 // function getYearUrls (start, end) {
 // 	const urlListRoot = "http://www.imdb.com/search/title?sort=moviemeter,asc&start=";
@@ -23,16 +24,63 @@ function getMovieLinks (body) {
 		   	nodes = $(".title>a"),
 			links = [].map.call(nodes, function(el){
 				el = $(el);
-				// var a = el.find("a");
 				return el[0].attribs.href;
 				// console.log(el[0].attribs);
 			});
 	return links;
 }
 
+function getCharacters (body) {
+	var charObj = {}
+	var $ = cheerio.load(body),
+			nodes = $(".character>div>a"),
+			actorNodes = $("[itemprop='actor']>a")
+			characters = [].map.call(nodes, function(el, i){
+				el = $(el);
+				var charName = el.html();
+				charObj[i] = { charName: charName };
+				return el.html();
+			}),
+			actors = [].map.call(actorNodes, function(el, i) {
+				var actorLink = el.attribs.href;
+				el = $(el);
+				var actorName = $(el.find("span")).html()
+				console.log(charObj[i], i)
+				if (charObj[i]) {
+					charObj[i].actor = { name: actorName,
+				 				 	 link: actorLink };
+				}
+				else {
+					charObj[i] = { charName: null };
+					charObj[i].actor = { name: actorName,
+				 				 	 link: actorLink };
+				}
+			})
+	console.log(charObj)
+	return characters;
+}
 
-getData(url, getMovieLinks)
-.then(links => {
-	console.log(links)
+var movieUrlRoot = "http://www.imdb.com/";
+
+getData("http://www.imdb.com/title/tt3499096/", getCharacters)
+.then(characters => {
+	// console.log(characters);
 });
+// var parent = $(".character>div>a").closest("td")
+// parent.prev
+// var closestActorCol = parent.siblings(".itemprop")
+// $(closestActorCol).find("a")
+// $(".character>div>a").find$("[itemprop='actor']");
+// var char = $(".character>div>a").closest('td').closest('td');
+// getData(url, getMovieLinks)
+// .then(links => {
+// 	console.log(links)
+// });
+
+
+	// /title/tt2869728/
+
+
+
+
 
