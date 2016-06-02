fe7246e3d4c13e674af3be3cd4193b1031f1a47d
Svinci131 <svinci131@gmail.com>
2016-06-02 17:01:57 -0400

    getting titlelinks

--- scripts/movieLists/get.js
+++ scripts/movieLists/get.js
@@ -1,26 +1,11 @@
 // 'use strict'
 
-// const request = require("request");
-// const cheerio = require("cheerio");
 
-// module.exports = function (url) {
-// 	return new Promise (function (resolve, reject) {
-// 		request(url, function (error, response, body) {
-// 		  if (!error) {
-// 		    var $ = cheerio.load(body),
-// 		    	text = $("pre").text();
-// 		    	resolve(text);
-// 		  }
-// 		  else {
-// 		  	reject(error);
-// 		  }
-		  
-// 		});
-// 	});
-// };
+const cheerio = require("cheerio");
+const getData = require("../getData");
 
 
-// function getYear (start, end) {
+// function getYearUrls (start, end) {
 // 	const urlListRoot = "http://www.imdb.com/search/title?sort=moviemeter,asc&start=";
 // 	var url = "urlListRoot+1&title_type=feature&year="+year+","+year;
 
@@ -29,3 +14,25 @@
 // 	}	
 
 // }
+var url = "http://www.imdb.com/search/title?sort=moviemeter,asc&start=51&title_type=feature&year=2016,2016";
+
+
+
+function getMovieLinks (body) {
+	var $ = cheerio.load(body),
+		   	nodes = $(".title>a"),
+			links = [].map.call(nodes, function(el){
+				el = $(el);
+				// var a = el.find("a");
+				return el[0].attribs;
+				// console.log(el[0].attribs);
+			});
+	return links;
+}
+
+
+getData(url, getMovieLinks)
+.then(links => {
+	console.log(links)
+});
+
